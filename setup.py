20a996819a1ccadf89acf4e3444666b2840e0438
Maher <maher.nasr@ete.inrs.ca>
2021-10-21 17:36:37 -0400

    update setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires= "==3.7",
+    python_requires= ">=3.7",
     install_requires=["numpy",
                       "scipy",
                       "vtk",

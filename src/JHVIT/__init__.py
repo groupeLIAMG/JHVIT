3c9a7a0777889bb1fbdcab3ad501da4164b31735
Maher <maher.nasr@ete.inrs.ca>
2021-10-21 22:43:18 -0400

    update init

--- src/JHVIT/__init__.py
+++ src/JHVIT/__init__.py
@@ -2,5 +2,5 @@
 """
 Code to perform seismic hypocenter location on tetrahedral meshes
 """
-from JHVIT.mesh import MSHReader
+
 from JHVIT.JHVI_Tetra import jntHypoVel_T, jntHyposlow_T, jntHypoVelPS_T, jntHyposlowPS_T, jointHypoVel_T, jointHypoVelPS_T

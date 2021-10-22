32e6d9064a0d38776291b4df84a07389693db570
Maher <maher.nasr@ete.inrs.ca>
2021-10-21 21:13:26 -0400

    fix bug in src

--- src/JHVIT/__init__.py
+++ src/JHVIT/__init__.py
@@ -2,3 +2,4 @@
 """
 Code to perform seismic hypocenter location on tetrahedral meshes
 """
+from JHVIT.JHVI_Tetra import jntHypoVel_T, jntHyposlow_T, jntHypoVelPS_T, jntHyposlowPS_T, jointHypoVel_T, jointHypoVelPS_T
